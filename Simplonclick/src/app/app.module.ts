c95a0966b13f986e704685c566bd85182e866f5f
Gosuhan <gosuhan@hotmail.fr>
2018-04-13 17:29:28 +0200

    Mise à jour des imports (AngularMaterial/FlexLayout...)

--- Simplonclick/src/app/app.module.ts
+++ Simplonclick/src/app/app.module.ts
@@ -1,8 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatInputModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatTableModule,
+  MatNativeDateModule,
+  MatListModule,
+  MatDialogModule,
+  MatSlideToggleModule,
+  MatTooltipModule,
+  MatSortModule,
+  MatSnackBarModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,7 +33,30 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatSortModule,
+    MatNativeDateModule,
+    MatListModule,
+    AppRoutingModule,
+    FormsModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
